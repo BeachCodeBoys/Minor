fa82ae2bfb4c3a271ff1e73cecd21deebf5cd2c2
user <user@user-VirtualBox>
2018-09-14 13:40:45 +0200

    Eerste opdracht

--- Realtime Systems/ARRTS-Assignment1_CCS_Project_Template/Source/main.c
+++ Realtime Systems/ARRTS-Assignment1_CCS_Project_Template/Source/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <FreeRTOS.h>
 #include <task.h>
+#include <inttypes.h>
 
 
 /*
@@ -21,11 +22,82 @@
      *  Good Luck!
      */
 
+void FunBlinkLed(void);
+void FunDelay(void);
+void FunPrint(void);
+void initClock(void);
+
+uint32_t klok = 0;
+volatile uint32_t ii;
+
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;  /* Stop watchdog timer */
+    //WDTCTL = WDTPW | WDTHOLD;  /* Stop watchdog timer */
+
+    //initClock();
+
+    klok = CS_getMCLK();
+
+    MAP_GPIO_setAsOutputPin(GPIO_PORT_P1,  GPIO_PIN0);
+
+    printf("%" PRIu32 "\n",klok);
+
+    FunBlinkLed();
+
+
+    while (1)
+    {
+
+    	FunBlinkLed();
+    	//FunPrint();
+    	FunDelay();
+
+
+
+
+
+    }//	*/
+
 
 
 
 	return 0;
 }
 
+void initClock(void)
+{
+	/* Halting WDT and disabling master interrupts */
+	    MAP_WDT_A_holdTimer();
+	    MAP_Interrupt_disableMaster();
+
+	    /* Set the core voltage level to VCORE1 */
+	    MAP_PCM_setCoreVoltageLevel(PCM_VCORE1);
+
+	    /* Set 2 flash wait states for Flash bank 0 and 1*/
+	    MAP_FlashCtl_setWaitState(FLASH_BANK0, 2);
+	    MAP_FlashCtl_setWaitState(FLASH_BANK1, 2);
+
+	    /* Initializes Clock System */
+	    MAP_CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_48);
+	    MAP_CS_initClockSignal(CS_MCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1 );
+	    MAP_CS_initClockSignal(CS_HSMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1 );
+	    MAP_CS_initClockSignal(CS_SMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1 );
+	    MAP_CS_initClockSignal(CS_ACLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+}
+
+void FunBlinkLed(void)
+{
+	MAP_GPIO_toggleOutputOnPin(GPIO_PORT_P1,  GPIO_PIN0);
+
+}
+
+void FunDelay(void)
+{
+	for(ii=0;ii<5000;ii++)
+	{
+	}
+}
+
+void FunPrint(void)
+{
+
+}
