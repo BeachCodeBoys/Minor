894e9584b141007869f9c0fe715c026544ab06b6
Habibi <mousalyoussef1@gmail.com>
2018-09-20 13:41:51 +0200

    opdr3

---
+++ Realtime Systems/ARRTS-Assignment3_CCS_Project_Template/Source/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <FreeRTOS.h>
+#include <task.h>
+
+
+/*
+ * ARTS-Lab-2016, HHs Electrotechniek, W.Muhammad
+ * Assignment 3- Project Template
+
+     */
+
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD;  /* Stop watchdog timer */
+
+
+
+	return 0;
+}
+
